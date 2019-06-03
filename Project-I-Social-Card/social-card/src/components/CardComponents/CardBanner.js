07f0902d91b32da7cbf2d34e5152ee4220088a0c
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-03 19:28:30 -0400

    card cont in app

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -6,10 +6,13 @@ import './Card.css';
 const CardBanner = () => {
     return (
 
-        <img src={"reactbackground.png"} alt="reactbg" />
+        <img src={"https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"} alt="reactbg" />
  
     );
     };
 
 
 export default CardBanner;
+
+
+
