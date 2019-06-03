d6ee299f100f3d5fa2fa265005df5d6d12ed2e03
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-03 17:30:54 -0400

    test

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -3,4 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import imgThumb from "./components/HeaderComponents/ImageThumbnail.js";
+// import imgThumb from "./components/HeaderComponents/HeaderContainer.js";
+
+
+
+<div className="App">
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
