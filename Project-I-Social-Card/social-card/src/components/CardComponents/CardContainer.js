8b57d162608bd7dae5b89c7e4f88781cbecb8164
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-04 00:20:31 -0400

    css

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
-
+import CardContent from './CardContent';
 
 
 
 
 const CardContainer = () => {
     return (
+
         <div>
-         <CardBanner />
-       </div>
+            <CardBanner />
+            
+            <CardContent />
+        </div>
+        
     );
 };
 
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+    return (
+        
+        <div class="CardCont">
+
+            <h3>Get started with React</h3>
+
+            <p>
+                React makes it painless to create interactive UIs.
+                Design simple views for each state in your application.
+            </p>
+
+            <h4>reactjs.org</h4>
+
+        </div>
+
+    );
+};
+
+
+export default CardContent;
