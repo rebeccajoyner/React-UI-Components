733268ddaf6b635f2df3c21bf0b4afdbb7922080
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-03 22:54:32 -0400

    divs renamed

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -6,8 +6,11 @@ import './Card.css';
 const CardBanner = () => {
     return (
 
+        <div class="ReactLogo">
+
         <img src={"https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"} alt="reactbg" />
  
+        </div>
     );
     };
 
