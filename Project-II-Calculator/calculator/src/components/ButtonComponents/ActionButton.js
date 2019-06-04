b9b6ec6097337c067d39376ec7e786278ed0a605
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-04 18:34:13 -0400

    numbutton

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = () => {
+    
+    return (
+
+        <div class="ActButton">
+
+            <h2>clear</h2>
+            
+            <h2>0</h2>
+
+        </div>
+
+    );
+};
+
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,29 @@
 import React from 'react';
 import './Button.css';
+
+
+
+const NumberButton = () => {
+
+    return (
+
+        <div class="NumButton">
+
+            <h3>7</h3>
+            <h3>8</h3>
+            <h3>9</h3>
+            <h3>4</h3>
+            <h3>5</h3>
+            <h3>6</h3>
+            <h3>1</h3>
+            <h3>2</h3>
+            <h3>3</h3>
+
+
+        </div>
+    )
+
+}
+
+
+export default NumberButton;
