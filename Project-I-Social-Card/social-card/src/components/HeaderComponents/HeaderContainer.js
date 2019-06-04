26e5e7a5597176a0287d3e492b5d876fafaf8b4d
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-03 21:58:47 -0400

    headcontent

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 
 
